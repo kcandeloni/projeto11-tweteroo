3e0a5fb17f5fbc593ec10249f2fff620dc8d5da1
kcandeloni <kcandeloni@gmail.com>
2022-08-26 01:34:31 -0300

    Setei a foto do usuário na resposta do get tweets + implementei a função que mandas os últimos 10 tweets

--- src/app.js
+++ src/app.js
@@ -9,9 +9,30 @@ app.use(cors());
 const users = [];
 const tweets = [];
 
+function getMesages(){
+  let mesages = [];
+  if(tweets.length > 10){
+    for(let i = tweets.length - 10; i < tweets.length; i++){
+      mesages.push(tweets[i]);
+    }
+  }
+  else {
+    mesages = tweets;
+  }
+  mesages.map(
+    tweet => {tweet.avatar = users.find(
+      user => user.username === tweet.username
+    ).avatar}
+  );
+
+  return mesages;
+}
+
 app.get('/tweets', (req, res)=> {
-  //
-    res.send(tweets);
+    const mesages = getMesages();
+    console.log(tweets);
+    console.log(mesages);
+    res.send(mesages);
 });
 
 app.post('/tweets', (req, res)=>{
@@ -22,13 +43,12 @@ app.post('/tweets', (req, res)=>{
    res.send('OK');
 });
 
-app.post('/sing-up', (req, res)=>{
+app.post('/sign-up', (req, res)=>{
     users.push({
       id: users.length + 1,
       ...req.body
     });
     res.send('OK');
 });
-
 
 app.listen(5000);
